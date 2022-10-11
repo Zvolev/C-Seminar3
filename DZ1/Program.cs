788a343d8141bd7a62b9c9cfe227f34ebae39b6e
Evgenii <zvolev@mail.ru>
2022-10-11 21:49:27 +0300

    Добавлена первая задача

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -3,3 +3,26 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+Start:
+
+Console.Write("Введите целое число >- ");
+long number = Convert.ToInt64(Console.ReadLine());
+
+void Palindrom(long num)
+{
+    long avers = num;
+    long revers = avers % 10;
+    while (avers > 10)
+    {
+        avers = avers / 10;
+        revers = revers * 10 + avers % 10;
+    }
+    if (num == revers)
+    {
+        Console.WriteLine($"Введенное число {num} является палиндромом");
+    }
+    else Console.WriteLine($"Введенное число {num} не является палиндромом");
+}
+Palindrom(number);
+Console.WriteLine();
+goto Start;
