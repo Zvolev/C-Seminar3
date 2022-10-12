f5ecb1210af87897f3699b7ed1afd09be6b0f518
Evgenii <zvolev@mail.ru>
2022-10-12 21:16:44 +0300

    Задача3 навел порядок

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,5 +1,4 @@
-﻿
-/* напишите программу на вход которой приходит два числа и 
+﻿/* Напишите программу на вход которой приходит два числа и 
 она находит растояние между ними в 2d пространстве*/
 Console.Clear();
 
@@ -13,7 +12,7 @@ int Prompt(string message)
     return number;
 }
 
-int [] InputPoint(int point)
+int[] InputPoint(int point)
 {
     int[] answer = new int[2];
     answer[X] = Prompt($"введите х {point} -> ");
@@ -21,14 +20,14 @@ int [] InputPoint(int point)
     return answer;
 }
 
-int Power2(int arg)  
+int Power2(int arg)
 {
     return arg * arg; // Math.Pow(arg, 2) - возведение в степень
 }
 
-int [] p1 = InputPoint(1);
-int [] p2 = InputPoint(2);
+int[] p1 = InputPoint(1);
+int[] p2 = InputPoint(2);
 
-double lenght = Math.Sqrt(Power2(p1[X] - p2[X]) + Power2(p1[Y]-p2[Y])); //Math.Sqrt - квадратный корень
+double lenght = Math.Sqrt(Power2(p1[X] - p2[X]) + Power2(p1[Y] - p2[Y])); //Math.Sqrt - квадратный корень
 
 System.Console.WriteLine($"{lenght:f2}");
